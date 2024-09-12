b1b09765121b6bece6ed845be2d2096cb06204ea
wy8933 <wy8933@rit.edu>
2024-09-12 12:08:42 -0400

    Completed the base enemy

--- Assets/Scripts/AI/CollisionHandler/BaseCollisionHandler.cs
+++ Assets/Scripts/AI/CollisionHandler/BaseCollisionHandler.cs
@@ -1,18 +1,24 @@
 using Godot;
 using System;
 
-public partial class SlimeMovement : Node
+public partial class BaseCollisionHandler : Node
 {
-	public Node player = get_node();
-	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		 Connect("body_entered", this, nameof(OnBodyEntered));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		if (body is Player) 
+		{
+			GD.Print("player collided");
+		}
+		else
+		{
+			GD.Print("collided with other gameobject");
+		}
 	}
 }
---
+++ Assets/Scripts/AI/Movement/SlimeMovement.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class SlimeMovement : Node
+{
+	// Speed of the enemy
+	[Export] public float Speed = 100f;
+	
+	private Node2D player;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		 player = GetNodeOrNull<Node2D>("");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Movement();
+	}
+	
+	private void Movement(){
+		Vector2 targetPosition;
+		if (player != null)
+		{
+			targetPosition = player.Position;
+		}
+		else
+		{
+			targetPosition = new Vector2(0, 0);
+		}
+		MoveTowardsTarget(targetPosition, delta);
+	}
+	
+	private void MoveTowardsTarget(Vector2 targetPosition, float delta)
+	{
+		Vector2 direction = (targetPosition - Position).Normalized();
+		Position += direction * Speed * delta;
+	}
+}
