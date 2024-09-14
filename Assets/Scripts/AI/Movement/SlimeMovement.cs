0b410946410888be72d61aa62944f2b47d602f26
wy8933 <wy8933@rit.edu>
2024-09-14 13:16:15 -0400

    Added HUD to show player data

--- Assets/Scripts/AI/Movement/SlimeMovement.cs
+++ Assets/Scripts/AI/Movement/SlimeMovement.cs
@@ -7,7 +7,6 @@ public partial class SlimeMovement : Node2D
 	[Export] public float Speed = 100f;
 	
 	private Node2D player;
-	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
