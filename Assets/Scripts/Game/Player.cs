e1dfe468750760118224dd51f22853ca3a724afe
Patrick <prd9410@g.rit.edu>
2024-09-14 14:13:42 -0400

    Added player to game scene. Needs a little work

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,23 +1,29 @@
 using Godot;
 using System;
 
-enum FACING_DIRECTION { Left, Right }
+enum FACING_DIRECTION { Left, Right, Up, Down, UpLeft, UpRight, DownLeft, DownRight }
 
 public partial class Player : Area2D
 {
 	// Fields
 	private Vector2 heading;
 	private Vector2 velocity;
-	private float maxSpeed = 25.0f;
+	private float maxSpeed = 50.0f;
 	private float speed;
 	public Sprite2D sprite;
 	private FACING_DIRECTION facing;
+
+	private int health;
+	private bool dead;
+	public bool isDead { get { return dead; } }
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		heading = new Vector2();
 		velocity = new Vector2();
 		facing = FACING_DIRECTION.Right;
+		health = 3;
+		dead = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -56,13 +62,50 @@ public partial class Player : Area2D
 	}
 	private void updateFacing()
 	{
-		if (heading.X > 0)
+		if (heading.X > 0 && heading.Y == 0)
 		{
 			facing = FACING_DIRECTION.Right;
 		}
-		else if (heading.X < 0)
+		else if (heading.X < 0 && heading.Y == 0)
 		{
 			facing = FACING_DIRECTION.Left;
 		}
+		else if (heading.X == 0 && heading.Y > 0)
+		{
+			facing = FACING_DIRECTION.Up;
+		}
+		else if (heading.X == 0 && heading.Y < 0)
+		{
+			facing = FACING_DIRECTION.Down;
+		}
+		else if(heading.X > 0 && heading.Y > 0)
+		{
+			facing = FACING_DIRECTION.UpRight;
+		}
+		else if(heading.X > 0 && heading.Y < 0)
+		{
+			facing = FACING_DIRECTION.DownRight;
+		}
+		else if(heading.X < 0 && heading.Y > 0)
+		{
+			facing = FACING_DIRECTION.UpLeft;
+		}
+		else if(heading.X < 0 && heading.Y < 0)
+		{
+			facing = FACING_DIRECTION.DownLeft;
+		}
+	}
+	private void takeDamage(int damage)
+	{
+		if (health > 0)
+		{
+			health -= damage;
+			if (health < 0)
+			{
+				health = 0;
+				dead = true;
+			}
+		}
+
 	}
 }
