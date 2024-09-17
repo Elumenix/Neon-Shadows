a4e5adbdce0d924d88fcfd2c5d11bac9b65f8905
wy8933 <wy8933@rit.edu>
2024-09-17 12:53:13 -0400

    Slime correct finds player and follow it

--- Assets/Scripts/Game/Camera.cs
+++ Assets/Scripts/Game/Camera.cs
@@ -6,10 +6,18 @@ public partial class Camera : Camera2D
 	
 	public override void _Ready()
 	{
+
 	}
 
 	
 	public override void _Process(double delta)
 	{
+
+	}
+
+	public void ScreenShake() { }
+
+	public void ScreenShake(float power) { 
+	
 	}
 }
