3d4e89a234500b00de67f7a299de4172aa5b8d8d
wy8933 <wy8933@rit.edu>
2024-09-18 18:55:15 -0400

    added camera shake when enemy collide with player

--- Assets/Scripts/AI/BaseEnemyAI.cs
+++ Assets/Scripts/AI/BaseEnemyAI.cs
@@ -94,5 +94,6 @@ public partial class BaseEnemyAI : CharacterBody2D
     public void HandlePlayerCollison()
     {
         GD.Print("Enemy collided with the player!");
+        Camera.Instance.StartShakeCamera(0.1f, 10);
     }
 }
