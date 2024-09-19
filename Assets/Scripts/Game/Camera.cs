5ceb2743ff649aa33e5b6f1c15d24bb8f85f51fc
Samera V <sev8971@rit.edu>
2024-09-19 13:43:23 -0400

    camera zoom

--- Assets/Scripts/Game/Camera.cs
+++ Assets/Scripts/Game/Camera.cs
@@ -3,61 +3,61 @@ using System;
 
 public partial class Camera : Camera2D
 {
-    public static Camera Instance { get; private set; }
+	public static Camera Instance { get; private set; }
 
-    private Random _random = new Random();
-    private Vector2 _originalPosition;
+	private Random _random = new Random();
+	private Vector2 _originalPosition;
 
-    private float _shakeDuration = 1f;
-    private float _shakeIntensity = 10f;
+	private float _shakeDuration = 1f;
+	private float _shakeIntensity = 10f;
 
-    public override void _Ready()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else { 
-            QueueFree();
-        }
-       
-        _originalPosition = Position;
-    }
+	public override void _Ready()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else { 
+			QueueFree();
+		}
+	   
+		_originalPosition = Position;
+	}
 
-    public override void _Process(double delta)
-    {
-        ShakeCamera(delta);
-    }
+	public override void _Process(double delta)
+	{
+		ShakeCamera(delta);
+	}
 
-    public void StartShakeCamera(float duration, float intensity)
-    {
-        _shakeDuration = duration;
-        _shakeIntensity = intensity;
-        DragHorizontalEnabled = false;
-        DragVerticalEnabled = false;
-        GD.Print(_shakeDuration);
-    }
+	public void StartShakeCamera(float duration, float intensity)
+	{
+		_shakeDuration = duration;
+		_shakeIntensity = intensity;
+		DragHorizontalEnabled = false;
+		DragVerticalEnabled = false;
+		GD.Print(_shakeDuration);
+	}
 
-    public void ShakeCamera(double delta) {
-        // check if the shake time is over, if not shake the camera
-        if (_shakeDuration > 0)
-        {
-            float offsetX = (float)(_random.NextDouble() * 2 - 1) * _shakeIntensity;
-            float offsetY = (float)(_random.NextDouble() * 2 - 1) * _shakeIntensity;
+	public void ShakeCamera(double delta) {
+		// check if the shake time is over, if not shake the camera
+		if (_shakeDuration > 0)
+		{
+			float offsetX = (float)(_random.NextDouble() * 2 - 1) * _shakeIntensity;
+			float offsetY = (float)(_random.NextDouble() * 2 - 1) * _shakeIntensity;
 
-            Position = _originalPosition + new Vector2(offsetX, offsetY);
+			Position = _originalPosition + new Vector2(offsetX, offsetY);
 
-            _shakeDuration -= (float)delta;
+			_shakeDuration -= (float)delta;
 
 
-            // Reset the camera location when shake is over
-            if (_shakeDuration <= 0)
-            {
-                Position = _originalPosition;
+			// Reset the camera location when shake is over
+			if (_shakeDuration <= 0)
+			{
+				Position = _originalPosition;
 
-                DragHorizontalEnabled = true;
-                DragVerticalEnabled = true;
-            }
-        }
-    }
+				DragHorizontalEnabled = true;
+				DragVerticalEnabled = true;
+			}
+		}
+	}
 }
