3bae57b5c29db3f0ef3e408410fb96b48163222c
Patrick Doherty <prd9410@rit.edu>
2024-09-20 18:40:26 -0400

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit b466ce01d909d44ee6534bf858636bcab434e15d, reversing
    changes made to 1660efb9d4e495968959065e68e0603c91a91a09.

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -4,26 +4,26 @@ using System.Collections.Generic;
 
 public partial class HUDManager : Control
 {
-	public static HUDManager Instance { get; private set; }
+    public static HUDManager Instance { get; private set; }
 
-	[Export]
-	public Godot.Collections.Array<TextureRect> HealthIconList;
+    [Export]
+    public Godot.Collections.Array<TextureRect> HealthIconList;
 
-	public override void _Ready()
-	{
-		Instance = this;
-	}
+    public override void _Ready()
+    {
+        Instance = this;
+    }
 
-	public void IncreasePlayerHp() {
-		foreach (TextureRect image in HealthIconList) {
-			if (!image.Visible) { 
-				image.Visible = true;
-				return;
-			}
-		}
-	}
-	public void DescreasePlayerHp()
-	{
-		HealthIconList[HealthIconList.Count - 1].Visible = false;
-	}
+    public void IncreasePlayerHp() {
+        foreach (TextureRect image in HealthIconList) {
+            if (!image.Visible) { 
+                image.Visible = true;
+                return;
+            }
+        }
+    }
+    public void DescreasePlayerHp()
+    {
+        HealthIconList[HealthIconList.Count - 1].Visible = false;
+    }
 }
