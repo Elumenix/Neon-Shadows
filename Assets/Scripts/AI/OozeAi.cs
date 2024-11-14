e7a429a8f6e0014bb87c560db832b4fcdb31b1c6
Weijie Ye (RIT Student) <wy8933@rit.edu>
2024-11-14 11:58:59 -0500

    Update OozeAi.cs

--- Assets/Scripts/AI/OozeAi.cs
+++ Assets/Scripts/AI/OozeAi.cs
@@ -3,5 +3,62 @@ using System;
 
 public partial class OozeAi : BaseEnemyAI
 {
-	
+	 [Export] private float lungeSpeed = 200.0f;
+	[Export] private float lungeRange = 100.0f;
+	[Export] private float lungeDuration = 0.5f;
+	private Timer lungeTimer;
+	private bool isLunging = false;
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		float randomScale = (float)GD.RandRange(0.9, 1.1);
+		Scale = new Vector2(randomScale, randomScale);
+		Modulate = new Color(0, 0, (float)GD.RandRange(0.3, 0.7));
+
+		lungeTimer = new Timer();
+		AddChild(lungeTimer);
+		lungeTimer.WaitTime = lungeDuration;
+		lungeTimer.OneShot = true;
+		lungeTimer.Timeout += EndLunge;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_player != null && !isLunging && Position.DistanceTo(_player.Position) < lungeRange)
+		{
+			StartLunge();
+		}
+		else if (!isLunging)
+		{
+			base._PhysicsProcess(delta);
+		}
+	}
+
+	private void StartLunge()
+	{
+		isLunging = true;
+		_usePathFinding = false;
+		_shouldMove = true;
+		Velocity = (GlobalPosition.DirectionTo(_player.GlobalPosition) * lungeSpeed);
+		_iFrames = lungeDuration;
+		lungeTimer.Start();
+	}
+
+	private void EndLunge()
+	{
+		isLunging = false;
+		_usePathFinding = true;
+		_iFrames = 0;
+		Velocity = Vector2.Zero;
+	}
+
+	public override void TakeDamage(int damageAmount)
+	{
+		if (!isLunging)
+		{
+			base.TakeDamage(damageAmount);
+		}
+	}
 }
