eed99d968535a0e7556849da8321c5ea0690453d
Weijie Ye (RIT Student) <wy8933@rit.edu>
2024-11-23 12:48:52 -0500

    Fixed Ooze fall didn't count as death

--- Assets/Scripts/AI/OozeAi.cs
+++ Assets/Scripts/AI/OozeAi.cs
@@ -12,12 +12,12 @@ public partial class OozeAi : BaseEnemyAI
 	private bool _isLungeCooldown = false;
 	[Export] private AnimationPlayer _animationPlayer;
 	private Timer _ZIndexTimer;
-    private bool _isSpawning;
-    public override void _Ready()
+	private bool _isSpawning;
+	public override void _Ready()
 	{
 		base._Ready();
 
-        float randomScale = (float)GD.RandRange(0.8, 2);
+		float randomScale = (float)GD.RandRange(0.8, 2);
 		Scale = new Vector2(randomScale, randomScale);
 		Modulate = new Color(0, 0, (float)GD.RandRange(0.3, 0.7));
 
@@ -32,65 +32,65 @@ public partial class OozeAi : BaseEnemyAI
 		_ZIndexTimer = GetNode<Timer>("ZIndexTimer");
 		_ZIndexTimer.Timeout += ChangeZIndex;
 
-        _oneSecTimer = GetNode<Timer>("OneSecTimer");
-        _oneSecTimer.Timeout += OnOneSecTimerFinished;
-        _shouldMove = false;
-        _isSpawning = true;
-        Spawn();
-    }
+		_oneSecTimer = GetNode<Timer>("OneSecTimer");
+		_oneSecTimer.Timeout += OnOneSecTimerFinished;
+		_shouldMove = false;
+		_isSpawning = true;
+		Spawn();
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        if (IsOnPlatform()&&!_isSpawning)
-        {
-            if (_player != null && !_isLungeCooldown && GlobalPosition.DistanceTo(_player.GlobalPosition) < _lungeRange)
-            {
-                StartLunge();
-            }
-            else
-            {
-                UpdateNavigationTarget();
-                Vector2 nextPathPosition = nextPathPosition = _navigationAgent.GetNextPathPosition();
-                if (_navigationAgent.DistanceToTarget() < _detectionRange)
-                {
-                    _shouldMove = true;
-                }
-                else
-                {
-                    _shouldMove = false;
-                }
+	public override void _PhysicsProcess(double delta)
+	{
+		if (IsOnPlatform()&&!_isSpawning)
+		{
+			if (_player != null && !_isLungeCooldown && GlobalPosition.DistanceTo(_player.GlobalPosition) < _lungeRange)
+			{
+				StartLunge();
+			}
+			else
+			{
+				UpdateNavigationTarget();
+				Vector2 nextPathPosition = nextPathPosition = _navigationAgent.GetNextPathPosition();
+				if (_navigationAgent.DistanceToTarget() < _detectionRange)
+				{
+					_shouldMove = true;
+				}
+				else
+				{
+					_shouldMove = false;
+				}
 
-                if (_navigationAgent.IsNavigationFinished())
-                {
-                    nextPathPosition = GlobalPosition;
-                }
+				if (_navigationAgent.IsNavigationFinished())
+				{
+					nextPathPosition = GlobalPosition;
+				}
 
-                MoveTowardsTarget(nextPathPosition, delta);
-                PlayWalkAnimation(GlobalPosition.DirectionTo(nextPathPosition));
-            }
+				MoveTowardsTarget(nextPathPosition, delta);
+				PlayWalkAnimation(GlobalPosition.DirectionTo(nextPathPosition));
+			}
 
-            MoveAndSlide();
-        }
-
-
-    }
-
-    public void OnOneSecTimerFinished()
-    {
-        if (_animatedSprite.Animation == "Spawn")
-        {
-            _shouldMove = true;
-            _animatedSprite.Play("Walk_Down");
-            _lungeCooldownTimer.Start();
-            _isSpawning = false;
-        }
-        else if (_animatedSprite.Animation.ToString().Substring(0, 5) == "Death") {
-            QueueFree();
-        }
-    }
+			MoveAndSlide();
+		}
 
 
-    private bool IsOnPlatform()
+	}
+
+	public void OnOneSecTimerFinished()
+	{
+		if (_animatedSprite.Animation == "Spawn")
+		{
+			_shouldMove = true;
+			_animatedSprite.Play("Walk_Down");
+			_lungeCooldownTimer.Start();
+			_isSpawning = false;
+		}
+		else if (_animatedSprite.Animation.ToString().Substring(0, 5) == "Death") {
+			QueueFree();
+		}
+	}
+
+
+	private bool IsOnPlatform()
 	{
 		return GetTree().GetNodesInGroup("PlatformArea")[0].GetNode<Area2D>("PlatformArea").OverlapsArea(GetNode<Area2D>("EdgeDetect"));
 	}
@@ -99,7 +99,7 @@ public partial class OozeAi : BaseEnemyAI
 	{
 		_isLunging = true;
 		_isLungeCooldown = true;
-        _usePathFinding = false;
+		_usePathFinding = false;
 		_shouldMove = true;
 		
 		Vector2 direction = GlobalPosition.DirectionTo(_player.GlobalPosition);
@@ -107,8 +107,8 @@ public partial class OozeAi : BaseEnemyAI
 		
 		_iFrames = _lungeDuration;
 		_lungeTimer.Start();
-        PlayLungeAnimation(direction);
-    }
+		PlayLungeAnimation(direction);
+	}
 
 	private void EndLunge()
 	{
@@ -117,174 +117,174 @@ public partial class OozeAi : BaseEnemyAI
 		Velocity = Vector2.Zero;
 		_lungeCooldownTimer.Start();
 
-        _isLunging = false;
+		_isLunging = false;
 
 		if (!IsOnPlatform())
 		{
-            _animationPlayer.Play("Fall");
-            GetNode<Timer>("ZIndexTimer").Start();
-        }
-    
-    }
+			_animationPlayer.Play("Fall");
+			GetNode<Timer>("ZIndexTimer").Start();
+		}
+	
+	}
 
 	private void ChangeZIndex() {
 		ZIndex = -1;
+		HandleDeath();
 	}
 
 
-    private void EndCooldown() {
-        _isLungeCooldown = false;
+	private void EndCooldown() {
+		_isLungeCooldown = false;
 
-    }
+	}
 
 
 	private void PlayLungeAnimation(Vector2 direction) {
-        direction = direction.Normalized();
-        if (direction.Y < -0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Lunge_UpRight");
-        }
-        else if (direction.Y < -0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Lunge_UpLeft");
-        }
-        else if (direction.Y > 0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Lunge_DownRight");
-        }
-        else if (direction.Y > 0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Lunge_DownLeft");
-        }
-        else if (direction.Y < -0.5f)
-        {
-            _animatedSprite.Play("Lunge_Up");
-        }
-        else if (direction.Y > 0.5f)
-        {
-            _animatedSprite.Play("Lunge_Down");
-        }
-        else if (direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Lunge_Left");
-        }
-        else if (direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Lunge_Right");
-        }
-    }
+		direction = direction.Normalized();
+		if (direction.Y < -0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Lunge_UpRight");
+		}
+		else if (direction.Y < -0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Lunge_UpLeft");
+		}
+		else if (direction.Y > 0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Lunge_DownRight");
+		}
+		else if (direction.Y > 0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Lunge_DownLeft");
+		}
+		else if (direction.Y < -0.5f)
+		{
+			_animatedSprite.Play("Lunge_Up");
+		}
+		else if (direction.Y > 0.5f)
+		{
+			_animatedSprite.Play("Lunge_Down");
+		}
+		else if (direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Lunge_Left");
+		}
+		else if (direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Lunge_Right");
+		}
+	}
 
-    private void PlayWalkAnimation(Vector2 direction)
-    {
-        direction = direction.Normalized();
-        if (direction.Y < -0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Walk_UpRight");
-        }
-        else if (direction.Y < -0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Walk_UpLeft");
-        }
-        else if (direction.Y > 0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Walk_DownRight");
-        }
-        else if (direction.Y > 0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Walk_DownLeft");
-        }
-        else if (direction.Y < -0.5f)
-        {
-            _animatedSprite.Play("Walk_Up");
-        }
-        else if (direction.Y > 0.5f)
-        {
-            _animatedSprite.Play("Walk_Down");
-        }
-        else if (direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Walk_Left");
-        }
-        else if (direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Walk_Right");
-        }
-    }
+	private void PlayWalkAnimation(Vector2 direction)
+	{
+		direction = direction.Normalized();
+		if (direction.Y < -0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Walk_UpRight");
+		}
+		else if (direction.Y < -0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Walk_UpLeft");
+		}
+		else if (direction.Y > 0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Walk_DownRight");
+		}
+		else if (direction.Y > 0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Walk_DownLeft");
+		}
+		else if (direction.Y < -0.5f)
+		{
+			_animatedSprite.Play("Walk_Up");
+		}
+		else if (direction.Y > 0.5f)
+		{
+			_animatedSprite.Play("Walk_Down");
+		}
+		else if (direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Walk_Left");
+		}
+		else if (direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Walk_Right");
+		}
+	}
 
-    private void PlayDeathAnimation(Vector2 direction)
-    {
-        direction = direction.Normalized();
-        if (direction.Y < -0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Death_UpRight");
-        }
-        else if (direction.Y < -0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Death_UpLeft");
-        }
-        else if (direction.Y > 0.5f && direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Death_DownRight");
-        }
-        else if (direction.Y > 0.5f && direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Death_DownLeft");
-        }
-        else if (direction.Y < -0.5f)
-        {
-            _animatedSprite.Play("Death_Up");
-        }
-        else if (direction.Y > 0.5f)
-        {
-            _animatedSprite.Play("Death_Down");
-        }
-        else if (direction.X < -0.5f)
-        {
-            _animatedSprite.Play("Death_Left");
-        }
-        else if (direction.X > 0.5f)
-        {
-            _animatedSprite.Play("Death_Right");
-        }
-    }
+	private void PlayDeathAnimation(Vector2 direction)
+	{
+		direction = direction.Normalized();
+		if (direction.Y < -0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Death_UpRight");
+		}
+		else if (direction.Y < -0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Death_UpLeft");
+		}
+		else if (direction.Y > 0.5f && direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Death_DownRight");
+		}
+		else if (direction.Y > 0.5f && direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Death_DownLeft");
+		}
+		else if (direction.Y < -0.5f)
+		{
+			_animatedSprite.Play("Death_Up");
+		}
+		else if (direction.Y > 0.5f)
+		{
+			_animatedSprite.Play("Death_Down");
+		}
+		else if (direction.X < -0.5f)
+		{
+			_animatedSprite.Play("Death_Left");
+		}
+		else if (direction.X > 0.5f)
+		{
+			_animatedSprite.Play("Death_Right");
+		}
+	}
 
-    protected override void HandleDeath()
-    {
-        Vector2 direction = GlobalPosition.DirectionTo(_player.GlobalPosition);
-        if (isDead)
-        {
-            PlayDeathAnimation(direction);
-            return;
-        }
-        isDead = true;
-        GameManager.Instance.EnemyDefeated();
-        PlayDeathAnimation(direction);
-        _shouldMove = false;
-        _isSpawning = true;
-        _oneSecTimer.Start();
-    }
+	protected override void HandleDeath()
+	{
+		Vector2 direction = GlobalPosition.DirectionTo(_player.GlobalPosition);
+		if (isDead)
+		{
+			PlayDeathAnimation(direction);
+			return;
+		}
+		isDead = true;
+		GameManager.Instance.EnemyDefeated();
+		PlayDeathAnimation(direction);
+		_shouldMove = false;
+		_isSpawning = true;
+		_oneSecTimer.Start();
+	}
 
 
-    public override void TakeDamage(int damageAmount)
-    {
-        if (isDead)
-            return;
+	public override void TakeDamage(int damageAmount)
+	{
+		if (isDead)
+			return;
 
-        _shouldMove = false;
-        if (!_isLunging)
-        {
-            base.TakeDamage(damageAmount);
-        }
-    }
+		_shouldMove = false;
+		if (!_isLunging)
+		{
+			base.TakeDamage(damageAmount);
+		}
+	}
 
-    protected void Spawn()
-    {
-        if (_animatedSprite == null)
-            _animatedSprite = GetNode<AnimatedSprite2D>("EnemySprite");
+	protected void Spawn()
+	{
+		if (_animatedSprite == null)
+			_animatedSprite = GetNode<AnimatedSprite2D>("EnemySprite");
 
-        _oneSecTimer.Start();
-        _animatedSprite.Play("Spawn");
-    }
+		_oneSecTimer.Start();
+		_animatedSprite.Play("Spawn");
+	}
 
 }
-
