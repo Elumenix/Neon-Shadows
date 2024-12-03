4be45789f8217223e65f05656137db20dd8c2f2f
Patrick Doherty <prd9410@rit.edu>
2024-12-03 00:59:09 -0500

    Ranged Area is mostly done

--- Assets/Scripts/Tutorial Scripts/SecondLedge.cs
+++ Assets/Scripts/Tutorial Scripts/SecondLedge.cs
@@ -1,46 +1,33 @@
 using Godot;
 using System;
 
-public partial class Reload : Area2D
+public partial class SecondLedge : Area2D
 {
-	private Label text;
 	private Label shootText;
+	private RichTextLabel fireText;
 	private bool showOnce = false;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		text = GetNode<Label>("%ReloadText");
 		shootText = GetNode<Label>("%ShootText");
+		fireText = GetNode<RichTextLabel>("%FireText");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	public void OnEnter(Node2D body)
-	{
-		if(body is Player)
-		{
-			Player temp = (Player)body;
-			temp.Reload();
-			shootText.Hide();
-			if (!showOnce)
-			{
-				showOnce = true;
-				text.Show();
-			}
-		}
-	}
 	public void OnExit(Node2D body)
 	{
 		if(body is Player)
 		{
-			Player temp = (Player)body;
-			temp.Reload();
-			if (text.Visible)
+			fireText.Hide();
+			if (!showOnce)
 			{
-				text.Hide();
+				shootText.Show();
+				showOnce = true;
 			}
 		}
 	}
